1f2ed89dae409780d2e2a88402f07c750c012844
Scott Werwath <swerwath@berkeley.edu>
2018-10-07 19:30:39 -0700

    Fixed import error

--- rad_classify/__init__.py
+++ rad_classify/__init__.py
@@ -1,4 +1,3 @@
-from .models import FastTextClassifier
 from .util import extract_impression, extract_clinical_history, extract_findings, SentenceTransformer, MapperTransformer
 from .preprocessing import ClearDroppedReports, SectionExtractor, SentenceTokenizer, PunctuationRemover, NegationMarker, StopWordRemover
 from .semantic_mapping import read_replacements, SemanticMapper, DateTimeMapper, AlphaNumRemover
